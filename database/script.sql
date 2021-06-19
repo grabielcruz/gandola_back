ccb816b73eb26a8bacae56dfa15f09983e5707f1
Luis Cruz <luisgrabielcruz@gmail.com>
2021-06-18 21:56:00 -0400

    able to add transactions

--- database/script.sql
+++ database/script.sql
@@ -4,17 +4,17 @@ DROP DATABASE gandola_soft;
 CREATE DATABASE gandola_soft;
 \c gandola_soft;
 
--- CREATE TYPE transaction_type AS ENUM ('output', 'input');
+CREATE TYPE transaction_type AS ENUM ('output', 'input');
 
 CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
-  -- type transaction_type,
+  type transaction_type,
   amount DECIMAL,
   executed TIMESTAMP
 );
 
-INSERT INTO transactions(amount, executed) 
-  VALUES ('55.23', '2021-06-17 04:05:06 -4:00');
+INSERT INTO transactions(type, amount, executed) 
+  VALUES ('input', '55.23', '2021-06-17 04:05:06 -4:00');
 
-INSERT INTO transactions(amount, executed) 
-  VALUES ('-23.00', '2021-05-09 08:09:12 -4:00');
+INSERT INTO transactions(type, amount, executed) 
+  VALUES ('output', '23.00', '2021-05-09 08:09:12 -4:00');
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -11,22 +13,46 @@ import (
 )
 
 type Transaction struct {
-	id int
-	amount float32
-	executed string
+	Id       int
+	Type     string
+	Amount   float32
+	Executed string
 }
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	transactions := []Transaction{}
+type Response struct {
+	Message string
+	Payload interface{}
+}
+
+var db *sql.DB
+
+func init() {
 	connStr := "user=postgres password=postgres host=localhost port=5432 dbname=gandola_soft"
-	db, err := sql.Open("postgres", connStr)
+	dbconn, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-
 	log.Print("Connected to database")
+	db = dbconn
+}
 
+func main() {
+	defer db.Close()
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	router.GET("/transactions", GetTransactions)
+	router.POST("/transactions", CreateTransaction)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Server working")
+}
+
+func GetTransactions(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	transactions := []Transaction{}
 	rows, err := db.Query("SELECT * FROM transactions;")
 	if err != nil {
 		log.Fatal(err)
@@ -34,18 +60,49 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	defer rows.Close()
 	for rows.Next() {
 		transaction := Transaction{}
-		if err := rows.Scan(&transaction.id, &transaction.amount, &transaction.executed); err != nil {
+		if err := rows.Scan(&transaction.Id, &transaction.Type, &transaction.Amount, &transaction.Executed); err != nil {
 			log.Fatal(err)
 		}
 		transactions = append(transactions, transaction)
-		// log.Printf("id %d amount %f executed %v", id, amount, executed)
 	}
-	
-	fmt.Fprintf(w, "Welcome!\n %v", transactions)
+	json_transactions, err := json.Marshal(transactions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json_transactions)
 }
 
-func main() {
-	router := httprouter.New()
-	router.GET("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
+func CreateTransaction(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	transaction := Transaction{}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "No se pudo leer el cuerpo de la petición")
+	}
+	err = json.Unmarshal(body, &transaction)
+	if err != nil {
+		fmt.Fprintf(w, "La data enviada no corresponde con una transacción")
+	}
+	query := fmt.Sprintf("INSERT INTO transactions(type, amount, executed) VALUES ('%v', '%v', '%v') RETURNING id, type, amount, executed;", transaction.Type, transaction.Amount, transaction.Executed)
+	inserted_transaction := Transaction{}
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&inserted_transaction.Id, &inserted_transaction.Type, &inserted_transaction.Amount, &inserted_transaction.Executed); err != nil {
+			log.Fatal(err)
+		}
+	}
+	response_data := Response{
+		Message: "Transacción creada existosamente",
+		Payload: inserted_transaction,
+	}
+	response, err := json.Marshal(response_data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
