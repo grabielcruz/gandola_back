6c9b0a9f2fcb49d8212ce3337f5585ed5cfbfa93
Luis Cruz <luisgrabielcruz@gmail.com>
2021-06-21 10:48:08 -0400

    adds new balance when creating a new Transaction

--- database/script.sql
+++ database/script.sql
@@ -9,7 +9,7 @@ CREATE TYPE transaction_type AS ENUM ('output', 'input');
 CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
   type transaction_type NOT NULL,
-  amount DECIMAL CHECK (amount > 0) NOT NULL,
+  amount DECIMAL(10, 2) CHECK (amount >= 0) NOT NULL,
   description TEXT,
   executed TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   prev_balance INT
@@ -17,7 +17,7 @@ CREATE TABLE transactions (
 
 CREATE TABLE balances (
   id SERIAL PRIMARY KEY,
-  balance DECIMAL CHECK (balance > 0) NOT NULL,
+  balance DECIMAL(10, 2) CHECK (balance >= 0) NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   prev_transaction INT
 );
@@ -25,8 +25,11 @@ CREATE TABLE balances (
 ALTER TABLE transactions ADD CONSTRAINT fk_prev_balance FOREIGN KEY (prev_balance) REFERENCES balances(id);
 ALTER TABLE balances ADD CONSTRAINT fk_prev_transaction FOREIGN KEY (prev_transaction) REFERENCES transactions(id);
 
--- INSERT INTO transactions(type, amount, description) 
---   VALUES ('input', '55.23', 'grasa');
+INSERT INTO balances(id, balance) VALUES ('0', '0'); -- first balance
+INSERT INTO transactions(id, type, amount, description) VALUES ('0', 'input', '0', 'transaction zero');
+
+UPDATE balances SET prev_transaction = '0' WHERE id = '0';
+UPDATE transactions SET prev_balance = '0' WHERE id = '0';
 
 -- INSERT INTO transactions(type, amount, description) 
 --   VALUES ('output', '23.00', 'bornes');
--- transactions/transactions.go
+++ transactions/transactions.go
@@ -11,12 +11,20 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type Balance struct {
+	Id int
+	Balance	float32
+	CreatedAt	string
+	PrevTransactionId int
+}
+
 type Transaction struct {
 	Id          int
 	Type        string
 	Amount      float32
 	Description string
 	Executed    string
+	PrevBalanceId int
 }
 
 type Response struct {
@@ -44,7 +52,7 @@ func GetTransactions(w http.ResponseWriter, r *http.Request, _ httprouter.Params
 	defer rows.Close()
 	for rows.Next() {
 		transaction := Transaction{}
-		if err := rows.Scan(&transaction.Id, &transaction.Type, &transaction.Amount, &transaction.Description, &transaction.Executed); err != nil {
+		if err := rows.Scan(&transaction.Id, &transaction.Type, &transaction.Amount, &transaction.Description, &transaction.Executed, &transaction.PrevBalanceId); err != nil {
 			log.Fatal(err)
 		}
 		transactions = append(transactions, transaction)
@@ -67,20 +75,51 @@ func CreateTransaction(w http.ResponseWriter, r *http.Request, _ httprouter.Para
 	if err != nil {
 		fmt.Fprintf(w, "La data enviada no corresponde con una transacción")
 	}
-	query := fmt.Sprintf("INSERT INTO transactions(type, amount, description) VALUES ('%v', '%v', '%v') RETURNING id, type, amount, description, executed;", transaction.Type, transaction.Amount, transaction.Description)
-	insertedTransaction := Transaction{}
+	
 	db := database.ConnectDB()
 	defer db.Close()
-	rows, err := db.Query(query)
+
+	prevBalance := Balance{}
+	getBalancesQuery := "SELECT * FROM balances ORDER BY id desc LIMIT 1"
+	balanceRows, err := db.Query(getBalancesQuery)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
-	for rows.Next() {
-		if err := rows.Scan(&insertedTransaction.Id, &insertedTransaction.Type, &insertedTransaction.Amount, &insertedTransaction.Description, &insertedTransaction.Executed); err != nil {
+	defer balanceRows.Close()
+	for balanceRows.Next() {
+		if err := balanceRows.Scan(&prevBalance.Id, &prevBalance.Balance, &prevBalance.CreatedAt, &prevBalance.PrevTransactionId); err != nil {
 			log.Fatal(err)
 		}
 	}
+	fmt.Println(prevBalance)
+
+	insertedTransaction := Transaction{}
+	insertTransactionQuery := fmt.Sprintf("INSERT INTO transactions(type, amount, description, prev_balance) VALUES ('%v', '%v', '%v', '%v') RETURNING id, type, amount, description, executed, prev_balance;", transaction.Type, transaction.Amount, transaction.Description, prevBalance.Id)
+	transactionRows, err := db.Query(insertTransactionQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer transactionRows.Close()
+	for transactionRows.Next() {
+		if err := transactionRows.Scan(&insertedTransaction.Id, &insertedTransaction.Type, &insertedTransaction.Amount, &insertedTransaction.Description, &insertedTransaction.Executed, &insertedTransaction.PrevBalanceId); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	var newBalanceAmount float32
+	if (insertedTransaction.Type == "input") {
+		newBalanceAmount = prevBalance.Balance + insertedTransaction.Amount
+	} else if (insertedTransaction.Type == "output") {
+		//TODO: check for negative balances
+		newBalanceAmount = prevBalance.Balance - insertedTransaction.Amount
+	}
+
+	insertBalanceQuery := fmt.Sprintf("INSERT INTO balances (balance, prev_transaction) VALUES ('%v', '%v');", newBalanceAmount, insertedTransaction.Id)
+	_, err = db.Query(insertBalanceQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	response_data := Response{
 		Message: "Transacción creada existosamente",
 		Payload: insertedTransaction,
