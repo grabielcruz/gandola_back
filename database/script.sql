b33ab8d84759c66a22e8669450ac8d158578c22e
Luis Cruz <luisgrabielcruz@gmail.com>
2021-06-20 22:31:33 -0400

    balances and transactions table. Next trigger

--- database/script.sql
+++ database/script.sql
@@ -8,14 +8,25 @@ CREATE TYPE transaction_type AS ENUM ('output', 'input');
 
 CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
-  type transaction_type,
-  amount DECIMAL,
+  type transaction_type NOT NULL,
+  amount DECIMAL CHECK (amount > 0) NOT NULL,
   description TEXT,
-  executed TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  executed TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  prev_balance INT
 );
 
-INSERT INTO transactions(type, amount, description) 
-  VALUES ('input', '55.23', 'grasa');
+CREATE TABLE balances (
+  id SERIAL PRIMARY KEY,
+  balance DECIMAL CHECK (balance > 0) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  prev_transaction INT
+);
 
-INSERT INTO transactions(type, amount, description) 
-  VALUES ('output', '23.00', 'bornes');
+ALTER TABLE transactions ADD CONSTRAINT fk_prev_balance FOREIGN KEY (prev_balance) REFERENCES balances(id);
+ALTER TABLE balances ADD CONSTRAINT fk_prev_transaction FOREIGN KEY (prev_transaction) REFERENCES transactions(id);
+
+-- INSERT INTO transactions(type, amount, description) 
+--   VALUES ('input', '55.23', 'grasa');
+
+-- INSERT INTO transactions(type, amount, description) 
+--   VALUES ('output', '23.00', 'bornes');
