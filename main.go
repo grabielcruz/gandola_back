68aaaf0fd5c7b6000c8413b99f2fd6d6b3b794b9
Luis Cruz <luisgrabielcruz@gmail.com>
2021-06-26 12:22:49 -0400

    main

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ func main() {
 	router.GET("/transactions", transactions.GetTransactions)
 	router.POST("/transactions", transactions.CreateTransaction)
 	router.PATCH("/transactions", transactions.PatchTransaction)
-	router.DELETE("/transactions", transactions.DeleteLastTransaction) //Delete only the last transaction
+	router.DELETE("/transactions", transactions.DeleteLastTransaction)
 	router.GET("/lasttransactionid", transactions.GetLastTransactionId) 
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+//Todo:use params on patch, explore reset reset serial on DeleteLastTransaction. Proceed with pendings as a regular crud
